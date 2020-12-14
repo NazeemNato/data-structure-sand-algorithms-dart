b92b51085fc06e7586ad6d04e04aec5a25c5b82f
Muhammed Nazeem <nazeemnob18@gmail.com>
2020-12-14 05:20:28 +0000

    Print the input in the same format.

---
+++ basic_input_output_3.dart
@@ -0,0 +1,28 @@
+// Write a code to get the input in the given format and print the output in the given format
+
+// Input Description:
+// First-line indicates two integers separated by space. Second-line indicates two integers separated by space. Third-line indicates two integers separated by space.
+
+// Output Description:
+// Print the input in the same format.
+
+// Sample Input :
+// 2 4
+// 2 4
+// 2 4
+
+// Sample Output :
+// 2 4
+// 2 4
+// 2 4
+
+import 'dart:io';
+
+void main(){
+var n1 = stdin.readLineSync();
+var n2 = stdin.readLineSync();
+var n3 = stdin.readLineSync();
+print(n1);
+print(n2);
+print(n3);
+}
--- main.dart
+++ main.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 void main(){
 var n1 = stdin.readLineSync();
 var n2 = stdin.readLineSync();
+var n3 = stdin.readLineSync();
 print(n1);
 print(n2);
+print(n3);
 }
